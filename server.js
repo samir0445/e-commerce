a58660ccc2bbfe504953983f40f909896be5518c
samir0445 <sb786095@gmail.com>
2025-07-25 20:23:26 +0530

    add server file

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-// hi
+import express from "express";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/" , (req,res)=>{
+    res.send("git op");
+})
+
+app.listen( process.env.PORT || 3000 ,()=>{
+    console.log(" server started bady");
+})
